e5a17a950691f0abe5d42f4ea4e69aa7e36daa11
Cisamu <76702667+cisamu123@users.noreply.github.com>
2022-05-31 02:44:56 +0700

    Add files via upload

---
+++ Jumping++/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
+
+namespace Jumping__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte procent = 100;
+            byte userprocent = 0;
+            Console.WriteLine("Starting Beating lvl...");
+            bool whil = true;
+            Thread.Sleep(1000);
+            while (whil)
+            {
+                userprocent++;
+                Thread.Sleep(700);
+                Console.WriteLine("Beating: " + userprocent + "%");
+                if (userprocent == procent)
+                {
+                     whil = false;
+                     Console.WriteLine("Level Completed!");
+                }
+            }
+        }
+    }
+}
